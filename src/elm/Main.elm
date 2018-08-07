c3c8d72cae536ded2bc43b6a41f8585ddde0d39b
nrdstrm <robert@nrdstrm.se>
2018-08-07 20:36:41 +0200

    base application structure.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,6 +3,60 @@ module Main exposing (main)
 import Html exposing (Html)
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
-    Html.text "Hello, World!"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { title : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { title = "Map" }, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text model.title ]
+        ]
--- src/static/index.js
+++ src/static/index.js
@@ -2,4 +2,4 @@ import "../../node_modules/leaflet/dist/leaflet.css";
 import "../../node_modules/leaflet/dist/leaflet.js";
 
 var Elm = require("../elm/Main");
-Elm.Main.embed(document.getElementById("main"));
+var app = Elm.Main.embed(document.getElementById("main"));
