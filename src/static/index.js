6e2340c4abda40b2918194602d74b9209663fa13
nrdstrm <robert@nrdstrm.se>
2018-08-07 20:21:46 +0200

    leaflet.

--- src/static/index.js
+++ src/static/index.js
@@ -1,2 +1,5 @@
+import "../../node_modules/leaflet/dist/leaflet.css";
+import "../../node_modules/leaflet/dist/leaflet.js";
+
 var Elm = require("../elm/Main");
 Elm.Main.embed(document.getElementById("main"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 var path = require("path");
+var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var autoprefixer = require("autoprefixer");
 
 module.exports = {
     entry: {
@@ -16,6 +18,28 @@ module.exports = {
     resolve: {
         extensions: [".js", ".elm"],
         modules: ["node_modules"],
+        alias: {
+            "./images/layers.png$": path.resolve(
+                __dirname,
+                "./node_modules/leaflet/dist/images/layers.png",
+            ),
+            "./images/layers-2x.png$": path.resolve(
+                __dirname,
+                "./node_modules/leaflet/dist/images/layers-2x.png",
+            ),
+            "./images/marker-icon.png$": path.resolve(
+                __dirname,
+                "./node_modules/leaflet/dist/images/marker-icon.png",
+            ),
+            "./images/marker-icon-2x.png$": path.resolve(
+                __dirname,
+                "./node_modules/leaflet/dist/images/marker-icon-2x.png",
+            ),
+            "./images/marker-shadow.png$": path.resolve(
+                __dirname,
+                "./node_modules/leaflet/dist/images/marker-shadow.png",
+            ),
+        },
     },
 
     module: {
@@ -30,12 +54,38 @@ module.exports = {
                     debug: true,
                 },
             }],
+        }, {
+            test: /\.(gif|svg|jpg|png)$/,
+            loader: "file-loader"
+        }, {
+            test: /\leaflet.css$/,
+            use: [{
+                loader: "style-loader",
+            }, {
+                loader: "css-loader",
+            }],
+        }, {
+            test: /\.css$/,
+            exclude: /\leaflet.css$/,
+            use: [{
+                loader: "style-loader",
+            }, {
+                loader: "css-loader",
+                options: {
+                    modules: true,
+                },
+            }],
         }],
 
         noParse: /\.elm$/
     },
 
     plugins: [
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [autoprefixer()]
+            }
+        }),
         new HtmlWebpackPlugin({
             template: "src/static/index.html",
             inject: "body",
